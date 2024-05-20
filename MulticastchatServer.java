a9df9d7e52f007c602ad7d5398d6ce45dad44971
Eris Kociaj <eris.kociaj@elev.ga.ntig.se>
2024-05-20 12:50:28 +0200

    2#: Uppdaterade MulticastchatServer för att förbättra kompatibiliteten med nyare Java-versioner och förbättra hanteringen av nätverkssgränssnitt. Modifierade argumentlängdskontrollen för att korrekt hantera ett enda argument för portnummerkonfiguration. Ersatte föråldrade joinGroup(InetAddress) och leaveGroup(InetAddress)-metoder med joinGroup(SocketAddress, NetworkInterface) och leaveGroup(SocketAddress, NetworkInterface) för att explicit ange nätverkssgränssnitt, vilket hanterar varningar för föråldring och förbättrar hanteringen av multicast-grupper.

--- MulticastchatServer.java
+++ MulticastchatServer.java
@@ -5,41 +5,39 @@ public class MulticastchatServer {
     public static void main(String args[]) throws Exception {
 
         // Default port number we are going to use
-        int portnumber = 5000;
+        int portnumber = 4446; // Changed default port number to 4446
 
-        if (args.length > 1) {
-
+        if (args.length > 0) { // Modified condition to check args.length > 0 instead of args.length >= 1
             portnumber = Integer.parseInt(args[0]);
-
-        } 
+        }
 
         // Create a MulticastSocket
-
-        MulticastSocket serverMulticastSocket =
-
-        new MulticastSocket(portnumber);
-
-        System.out.println("MulticastSocket is created at port " + portnumber); 
+        MulticastSocket serverMulticastSocket = new MulticastSocket(portnumber);
+        System.out.println("MulticastSocket is created at port " + portnumber);
 
         // Determine the IP address of a host, given the host name
         InetAddress group = InetAddress.getByName("225.4.5.6");
 
-        // getByName- returns IP address of given host
-        serverMulticastSocket.joinGroup(group); 
+        // getByName - returns IP address of given host
+        // Join the multicast group using a specified network interface
+        NetworkInterface networkInterface = NetworkInterface.getNetworkInterfaces().nextElement(); // This gets the first network interface, consider specifying if necessary
+        serverMulticastSocket.joinGroup(new InetSocketAddress(group, portnumber), networkInterface);
         System.out.println("joinGroup method is called...");
 
-        boolean infinite = true; 
+        boolean infinite = true;
         // Continually receives data and prints them
-
         while (infinite) {
+            byte buf[] = new byte[1024];
+            DatagramPacket data = new DatagramPacket(buf, buf.length);
+            serverMulticastSocket.receive(data);
+            String msg = new String(data.getData(), 0, data.getLength()).trim();
+            System.out.println("Message received from client = " + msg);
+        }
 
-            byte buf[] = new byte[1024]; 
-            DatagramPacket data = new DatagramPacket(buf, buf.length); 
-            serverMulticastSocket.receive(data); 
-            String msg = new String(data.getData()).trim(); 
-            System.out.println("Message received from client = " + msg); 
-        } 
+        // Leave the multicast group on a specified network interface
+        serverMulticastSocket.leaveGroup(new InetSocketAddress(group, portnumber), networkInterface);
+        System.out.println("Left multicast group.");
 
-        serverMulticastSocket.close(); 
+        serverMulticastSocket.close();
     }
 }
